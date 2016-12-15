437f814530a7356a6c4d72946455bbd1ebce68a1
BoguskiAdam <aboguski93@gmail.com>
2016-12-15 04:41:02 -0800

    MongoDb - Fix importowania

--- Dokumenty/ImportBookRatings.sh
+++ Dokumenty/ImportBookRatings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Importowanie BookRatings - Start"
 tar -xf BookRatings.json.tar.gz
-mongoimport --db AiProjekt --collection book --file BookRatings.json
+mongoimport --db AiProjekt --collection book_ratings --file BookRatings.json
 rm BookRatings.json
 echo "Importowanie BookRatings - Stop"
